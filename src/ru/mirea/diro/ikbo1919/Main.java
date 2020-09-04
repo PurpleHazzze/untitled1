5f3acf95653fa9478985ec474d68285ef6822dbb
ejhdf <fdhdfhdf>
2020-09-04 13:47:30 +0300

    start project

---
+++ src/ru/mirea/diro/ikbo1919/Main.java
@@ -0,0 +1,27 @@
+package ru.mirea.diro.ikbo1919;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    scanner.next()
+
+	int a = 10;
+	float b = 20.56f;
+	boolean c = false;
+
+	int[] d = new int[10];
+	String string = new String();
+	String string2 = "dfg ffg";
+	//est metodi
+
+	String[] stringarray = string2.split(" ");
+	for (String v : stringarray) {
+	    System.out.println(v);
+
+    }
+
+    }
+}
